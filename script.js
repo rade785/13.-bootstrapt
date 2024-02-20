09568a7a40ef95b254b5363fc4e6fb3f8eac06d8
rade785 <radefutbaler@gmail.com>
2024-02-20 18:02:15 +0100

    Changes to be committed: 	modified:   index.html 	modified:   script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let register = document.getElementById("register");
+let modalTitle = document.getElementById("exampleModalLabel");
+let modalFooter = document.getElementsByClassName("modal-footer")[0];
+let modalBody = document.getElementsByClassName("modal-body")[0];
+
+register.addEventListener("click", () => {
+    
+})
